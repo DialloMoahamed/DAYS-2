cd19e380e2872b16de189aa9fa27623b6cbd66e5
DialloMoahamed <mohamedattahirdiallo@gmail.com>
2024-09-24 23:41:33 +0100

    Exercice 2 terminer

---
+++ Exo2.js
@@ -0,0 +1,51 @@
+// Réponse numero 1
+console.log("The quote 'There is no exercise better for the heart than reaching down and lifting people up.' by John Holmes teaches us to help one another.");
+
+
+// Réponse numero 2
+console.log("Love is not patronizing and charity isn't about pity, it is about love. Charity and love are the same -- with charity you give love, so don't just give money but reach out your hand instead.");
+
+
+// Réponse numero 3
+let num = '10';
+console.log(parseInt(num));
+
+
+// Réponse numero 4
+let nume = '9.81'
+let numFloat = parseFloat(num)
+
+console.log(numFloat);
+
+
+// Réponse numero 5
+let word1 = "Python";
+console.log(word1.includes("on"))
+let word2 = "jargon";
+console.log(word2.includes("on"));
+
+
+// Réponse numero 6
+let sentences = "J'espère que ce cours n'est pas plein de jargon ."
+console.log(sentences.includes("jargon"));
+
+
+// Réponse numero 7
+let randomNum = Math.floor(Math.random() * 101)         
+console.log(randomNum);
+
+
+// Réponse numero 8 non trouver
+
+
+// Réponse numero 9
+let random = Math.floor(Math.random() * 256)         
+console.log(randomNum);
+
+
+// Réponse numero 10 non trouver
+
+
+// Réponse numero 11
+const paragraphe = "1 1 1 1 1\n2 1 2 4 8\n3 1 3 9 27\n4 1 4 16 64\n5 1 5 25 125"
+console.log(paragraphe);
